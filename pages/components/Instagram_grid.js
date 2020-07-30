92a479b8e30173c1649fb8b5f4eaef6d24c244b7
Callum <callum.git@outlook.com>
2020-07-30 13:02:40 +0100

    Added static content to instagram grid, started on btns

--- pages/components/Instagram_grid.js
+++ pages/components/Instagram_grid.js
@@ -1,38 +1,56 @@
 // import Link from 'next/link'
 
 export default function Instagram_grid() {
-    return (
-        <section className="instagram-grid">
-            <a href="https://www.google.com" target="_blank">
+    return (<section className="instagram">
+        <div className="instagram-grid">
+            <a href="https://www.instagram.com/p/CC6kM18KGf8/" target="_blank">
                 <div className="instagram-grid-item">
-                    <img src="https://source.unsplash.com/random/800x600"></img>
+                    <img src="/images/insta_example_1.jpg"></img>
                     <div className="instagram-grid-item-text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                        ⏳ #5MinsWith Talk Tonight ⁣ ⁣ Had a lovely chat with Dan from 🎧 @talktonightpod 💻 @talktonightblog ⁣ ⁣ We discussed all things music (and fangirled over Harry Styles) 👏⁣ ⁣ Named after Oasis’s song ‘Talk Tonight’ 🎶 both podcast and blog have amassed a great success in under a year⁣ ⁣ 🎸 Promoting and sharing new music, Talk Tonight has interviewed bands such as @citylightz @terraway.band @idlenoisemusic @redbrickband & more⁣ ⁣ Swipe left for the full interview ⏪⏪⁣ ⁣ Be sure to give them a follow if you aren’t already 🤘⁣ ⁣
                     </div>
                 </div>
             </a>
-            <a href="https://www.google.com" target="_blank">
+            <a href="https://www.instagram.com/p/CCEwODGqoIt/" target="_blank">
                 <div className="instagram-grid-item">
-                    <img src="https://source.unsplash.com/random/800x600"></img>
+                    <img src="/images/insta_example_2.jpg"></img>
+                    <div className="instagram-grid-item-text">
+                        Did you watch @interbrewmedia ‘s interview with @25th_hour_band ? Here is a little snippet ⤴️ . Thank you to everyone who participated in the challenge, we had so much fun reading through everyone’s memories 🧡 . Also a massive thank you to @interbrewmedia & @abitofeverythingpodcast for helping organise this 👏 looking forward to working with them again..... . New stuff coming out soon watch this space 🤘
+                    </div>
                 </div>
-            </a>            <a href="https://www.google.com" target="_blank">
+            </a>            <a href="https://www.instagram.com/p/CCEwODGqoIt/" target="_blank">
                 <div className="instagram-grid-item">
-                    <img src="https://source.unsplash.com/random/800x600"></img>
+                    <img src="/images/insta_example_3.jpg"></img>
+                    <div className="instagram-grid-item-text">
+                        Day 4 of the #FestivalChallenge ✨ Here we go!⁣ .⁣ We had a chat with @theverdictofficial ‘s drummer @gibson.walker 🥁 about his favourite concert experience and how lockdown is affecting the band ⁣ . ⁣ Did you know? 💭 The Verdict were recently featured in our “Unsigned” playlist be sure to check it out in the link in our bio ⤴️⁣ .⁣ They also recently released their new single “Fourth” on all streaming platforms go give it some love 🧡
+                    </div>
                 </div>
-            </a>            <a href="https://www.google.com" target="_blank">
+            </a>            <a href="https://www.instagram.com/p/CCEwODGqoIt/" target="_blank">
                 <div className="instagram-grid-item">
-                    <img src="https://source.unsplash.com/random/800x600"></img>
+                    <img src="/images/insta_example_4.jpg"></img>
+                    <div className="instagram-grid-item-text">
+                        Who else is missing festival season? 🙇‍♀‍ We know we are ⁣⁣ .⁣ A few weeks ago Tom from The Haciendas (@haciendasthe) talked to us about some of his favourite festival memories raging from crocodile floaty adventures at @kendalcalling 🐊, having beers with @mileskane 🍻, seeing @kasabianofficial at @fiberfib and many more! Swipe right to check out the FULL interview ▶️▶️▶️⁣⁣ .⁣ In honour of festival season we are running a week-long challenge on Instagram stories with The Interbrew (@interbrewmedia) and A Bit Of Everything Podcast (@abitofeverythingpodcast) 🤘
+                    </div>
                 </div>
-            </a>            <a href="https://www.google.com" target="_blank">
+            </a>            <a href="https://www.instagram.com/p/CCEwODGqoIt/" target="_blank">
                 <div className="instagram-grid-item">
-                    <img src="https://source.unsplash.com/random/800x600"></img>
+                    <img src="/images/insta_example_5.jpg"></img>
+                    <div className="instagram-grid-item-text">
+                        SHADE⁣⁣ ⭐️ @shade.mcr⁣⁣ 📍 Manchester, UK⁣⁣ .⁣⁣ .⁣⁣ .⁣⁣ SHADE Formed in Early 2019 and it wasn't long before they released they're first self titled EP SHADE. Since then they've been leaving their mark in venues all round Manchester with their raw but modern sound. The Band are a melodic powerhouse of catchy choruses and pounding drums. These lads are a must see.⁣⁣ .⁣⁣ 📸: @asupremeshot
+                    </div>
                 </div>
-            </a>            <a href="https://www.google.com" target="_blank">
+            </a>            <a href="https://www.instagram.com/p/CCEwODGqoIt/" target="_blank">
                 <div className="instagram-grid-item">
-                    <img src="https://source.unsplash.com/random/800x600"></img>
+                    <img src="/images/insta_example_6.jpg"></img>
+                    <div className="instagram-grid-item-text">
+                        Festival season is here 🎉⁣⁣⁣⁣⁣⁣ .⁣⁣⁣⁣⁣⁣ For today's feature we talked to @nana_music_official creator of @turnuptheworldfest which is being held on Instagram from the 12th to the 14th of June 🗓 (it ends TOMORROW)⁣⁣⁣⁣⁣⁣ .⁣⁣⁣⁣⁣⁣ 📢 “What has been taken away from artists and their fans is heartbreaking but is also an opportunity to connect them more directly than ever. With a story to tell from all around the globe, ‘Turn Up the World’ is a chance to stop worrying for a moment and feel understood, celebrated and joyful through music.”⁣⁣⁣⁣⁣⁣ .⁣⁣⁣⁣⁣⁣ When lockdown began most festivals and concerts were either cancelled or rescheduled to later in the year, resulting in major losses for artists. Not only financially 💸, but in terms of momentum 📈 and connecting with their fanbases 👯⁣⁣⁣⁣⁣⁣ .⁣⁣⁣⁣⁣⁣
+                    </div>
                 </div>
             </a>
-        </section>
+
+        </div>
+        <button className="btn-primary"><a href="https://www.instagram.com/the.quarantine.mixtape/" target="_blank" alt="Instagram profile">Show more</a></button>
+    </section>
     )
 }
 
