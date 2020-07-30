1c331e5d2ffab5ffff270ac040b95ab33002dcab
Callum <callum.git@outlook.com>
2020-07-30 14:05:53 +0100

    Added embedded content

--- pages/components/Social.js
+++ pages/components/Social.js
@@ -4,10 +4,11 @@ export default function Social() {
     return (
         <div className="social-group">
             <ul>
-                <li><a href="www.twitter.com" alt="twitter" target="_blank"><i className="lab la-twitter"></i></a></li>
-                <li><a href="www.facebook.com" alt="facebook" target="_blank"><i className="lab la-facebook-f"></i></a></li>
-                <li><a href="www.instagram.com" alt="instagram" target="_blank"><i className="lab la-instagram"></i></a></li>
-                <li><a href="www.youtube.com" alt="youtube" target="_blank"><i className="lab la-youtube"></i></a></li>
+                <li><a href="https://www.twitter.com" alt="twitter" target="_blank"><i className="lab la-twitter"></i></a></li>
+                <li><a href="https://www.facebook.com" alt="facebook" target="_blank"><i className="lab la-facebook-f"></i></a></li>
+                <li><a href="https://www.instagram.com" alt="instagram" target="_blank"><i className="lab la-instagram"></i></a></li>
+                <li><a href="https://www.youtube.com" alt="youtube" target="_blank"><i className="lab la-youtube"></i></a></li>
+                <li><a href="mailto:the.quarantine.mixtape@gmail.com" alt="email" target="_blank"><i className="las la-envelope"></i></a></li>
             </ul>
         </div>
     )
