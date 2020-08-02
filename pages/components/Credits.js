92aca7141dfda3793c4aeae97e47bcc3143b6b31
Callum <callum.git@outlook.com>
2020-08-02 19:03:11 +0100

    Added basic media queries

--- pages/components/Credits.js
+++ pages/components/Credits.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Credits() {
     return (
         <div className="credits">
-            <a href="https://www.scotdev.uk/" alt="Developer's website">Built by ScotDev</a>
+            <a href="https://www.scotdev.uk/" alt="Developer's website" target="_blank">Built by ScotDev</a>
         </div>
     )
 }
