36c90825724f61baefe2940274de90c929d87269
Callum <callum.git@outlook.com>
2020-08-02 18:35:25 +0100

    Added styling to Error component

--- pages/reviews/index.js
+++ pages/reviews/index.js
@@ -27,19 +27,19 @@ export default function reviews() {
                     </div>
                 </div>
                 <div className="review-grid-card">
-                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-grid-card-img"></img>
+                    <img src="/images/album2.jpg" className="review-grid-card-img"></img>
                     <div className="review-grid-card-text">
-                        <p className="review-grid-card-text-subtitle">Childish Gambino</p>
-                        <p className="review-grid-card-text-title">Awaken, My Love!</p>
-                        <span className="review-grid-card-text-date">11 July, 2020</span>
+                        <p className="review-grid-card-text-subtitle">Some Rapper</p>
+                        <p className="review-grid-card-text-title">Badass Rap Album</p>
+                        <span className="review-grid-card-text-date">12 January, 2019</span>
                     </div>
                 </div>
                 <div className="review-grid-card">
-                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-grid-card-img"></img>
+                    <img src="/images/album3.jfif" className="review-grid-card-img"></img>
                     <div className="review-grid-card-text">
-                        <p className="review-grid-card-text-subtitle">Childish Gambino</p>
-                        <p className="review-grid-card-text-title">Awaken, My Love!</p>
-                        <span className="review-grid-card-text-date">11 July, 2020</span>
+                        <p className="review-grid-card-text-subtitle">Fake Folk Band</p>
+                        <p className="review-grid-card-text-title">The Old Days</p>
+                        <span className="review-grid-card-text-date">3 March, 2020</span>
                     </div>
                 </div>
                 <div className="review-grid-card">
