24eacc011617bd9e3b44321fe61e0e10d5e3fabb
Callum <callum.git@outlook.com>
2020-08-02 18:27:55 +0100

    Added styling to Error component

--- pages/components/Error.js
+++ pages/components/Error.js
@@ -5,9 +5,9 @@ export default function Error({ statusCode }) {
     return (
         <div className="error">
             <h1 className="error-title">Page could not be loaded</h1>
-            <p>{statusCode}</p>
+            <p className="error-status-code">{statusCode}</p>
             <Link href="/">
-                <a alt="Return to homepage">Go Back <i className="las la-arrow-right"></i></a>
+                <a alt="Return to homepage" className="error-return-link">Go Back <i className="las la-arrow-right"></i></a>
             </Link>
         </div>
     )
--- pages/reviews/index.js
+++ pages/reviews/index.js
@@ -7,12 +7,11 @@ export default function reviews() {
             <title>thequarantinemixtape | Reviews</title>
         </Head>
 
-
         <section className="review">
             <h1 className="section-title">Album Reviews</h1>
             <div className="review-grid">
                 <div className="review-grid-card">
-                    <img src="/images/emily-rudolph-I-oARMjzXow-unsplash.jpg" className="review-img"></img>
+                    <img src="/images/emily-rudolph-I-oARMjzXow-unsplash.jpg" className="review-grid-card-img"></img>
                     <div className="review-grid-card-text">
                         <p className="review-grid-card-text-subtitle">Van Halen</p>
                         <p className="review-grid-card-text-title">Women and Children First</p>
@@ -20,7 +19,7 @@ export default function reviews() {
                     </div>
                 </div>
                 <div className="review-grid-card">
-                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-img"></img>
+                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-grid-card-img"></img>
                     <div className="review-grid-card-text">
                         <p className="review-grid-card-text-subtitle">Childish Gambino</p>
                         <p className="review-grid-card-text-title">Awaken, My Love!</p>
@@ -28,7 +27,7 @@ export default function reviews() {
                     </div>
                 </div>
                 <div className="review-grid-card">
-                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-img"></img>
+                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-grid-card-img"></img>
                     <div className="review-grid-card-text">
                         <p className="review-grid-card-text-subtitle">Childish Gambino</p>
                         <p className="review-grid-card-text-title">Awaken, My Love!</p>
@@ -36,7 +35,7 @@ export default function reviews() {
                     </div>
                 </div>
                 <div className="review-grid-card">
-                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-img"></img>
+                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-grid-card-img"></img>
                     <div className="review-grid-card-text">
                         <p className="review-grid-card-text-subtitle">Childish Gambino</p>
                         <p className="review-grid-card-text-title">Awaken, My Love!</p>
@@ -44,7 +43,7 @@ export default function reviews() {
                     </div>
                 </div>
                 <div className="review-grid-card">
-                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-img"></img>
+                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-grid-card-img"></img>
                     <div className="review-grid-card-text">
                         <p className="review-grid-card-text-subtitle">Childish Gambino</p>
                         <p className="review-grid-card-text-title">Awaken, My Love!</p>
@@ -52,7 +51,7 @@ export default function reviews() {
                     </div>
                 </div>
                 <div className="review-grid-card">
-                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-img"></img>
+                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-grid-card-img"></img>
                     <div className="review-grid-card-text">
                         <p className="review-grid-card-text-subtitle">Childish Gambino</p>
                         <p className="review-grid-card-text-title">Awaken, My Love!</p>
@@ -60,7 +59,7 @@ export default function reviews() {
                     </div>
                 </div>
                 <div className="review-grid-card">
-                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-img"></img>
+                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-grid-card-img"></img>
                     <div className="review-grid-card-text">
                         <p className="review-grid-card-text-subtitle">Childish Gambino</p>
                         <p className="review-grid-card-text-title">Awaken, My Love!</p>
@@ -68,7 +67,7 @@ export default function reviews() {
                     </div>
                 </div>
                 <div className="review-grid-card">
-                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-img"></img>
+                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-grid-card-img"></img>
                     <div className="review-grid-card-text">
                         <p className="review-grid-card-text-subtitle">Childish Gambino</p>
                         <p className="review-grid-card-text-title">Awaken, My Love!</p>
@@ -76,7 +75,7 @@ export default function reviews() {
                     </div>
                 </div>
                 <div className="review-grid-card">
-                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-img"></img>
+                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-grid-card-img"></img>
                     <div className="review-grid-card-text">
                         <p className="review-grid-card-text-subtitle">Childish Gambino</p>
                         <p className="review-grid-card-text-title">Awaken, My Love!</p>
@@ -84,7 +83,7 @@ export default function reviews() {
                     </div>
                 </div>
                 <div className="review-grid-card">
-                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-img"></img>
+                    <img src="/images/Childish_Gambino_-_Awaken,_My_Love!.png" className="review-grid-card-img"></img>
                     <div className="review-grid-card-text">
                         <p className="review-grid-card-text-subtitle">Childish Gambino</p>
                         <p className="review-grid-card-text-title">Awaken, My Love!</p>
