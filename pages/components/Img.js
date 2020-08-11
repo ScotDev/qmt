413bc93b1f7f2eb6d70e6e30cd00f56609b64d37
Callum <callum.git@outlook.com>
2020-08-11 14:02:11 +0100

    Enabled support for images

--- pages/components/Img.js
+++ pages/components/Img.js
@@ -6,7 +6,6 @@ export default function Img({ imgPath, imgClass, imgAlt }) {
 
     return (
         <img src={imgPath} style={{ objectFit: "cover" }} className={imgClass} alt={imgAlt}>
-            {/* <img src={devImgPath} style={{ objectFit: "cover" }} className={imgClass} alt={imgAlt}> */}
         </img>
     )
 
