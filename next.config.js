ce7bb7040826a994c2d194c9fa27e51ce48a04ea
Callum <callum.git@outlook.com>
2020-08-11 13:04:16 +0100

    Added env vars via vercel cli

--- next.config.js
+++ next.config.js
@@ -5,6 +5,7 @@ module.exports = {
     },
     publicRuntimeConfig: {
         // Will only be available on the client side
-        API_URL: "https://calm-depths-31916.herokuapp.com"
+        // API_URL: "https://calm-depths-31916.herokuapp.com"
+        API_URL: process.env.NEXT_PUBLIC_API_URL
     }
 }
