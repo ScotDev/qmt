3cd3a1cf00bc29a5b4cf6da10ef0c9f6e7edccf1
Callum <callum.git@outlook.com>
2020-08-11 14:53:03 +0100

    Updated base shareable url for social media links

--- pages/_app.js
+++ pages/_app.js
@@ -20,6 +20,7 @@ export default function MyApp({ Component, pageProps }) {
   return <>
     <Head>
       <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" />
+      <link rel="manifest" href="/manifest.webmanifest"></link>
     </Head>
 
 
