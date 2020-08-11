d28ec001316ee551130a8d32fde92bc402c0e8a0
Callum <callum.git@outlook.com>
2020-08-11 13:59:58 +0100

    Enabled support for images

--- pages/components/Img.js
+++ pages/components/Img.js
@@ -1,23 +1,12 @@
 
 import React from 'react'
 
-
-// import getConfig from 'next/config';
-
-// const { publicRuntimeConfig } = getConfig()
-
-// console.log(publicRuntimeConfig.API_URL)
-
-
 export default function Img({ imgPath, imgClass, imgAlt }) {
-
-    const devImgPath = "https://source.unsplash.com/random?waves/1920x1080"
-    // console.log(`${publicRuntimeConfig.API_URL}${imgPath}`)
 
 
     return (
-        // <img src={publicRuntimeConfig.API_URL + imgPath} className={imgClass} alt={imgAlt}>
-        <img src={devImgPath} style={{ objectFit: "cover" }} className={imgClass} alt={imgAlt}>
+        <img src={imgPath} style={{ objectFit: "cover" }} className={imgClass} alt={imgAlt}>
+            {/* <img src={devImgPath} style={{ objectFit: "cover" }} className={imgClass} alt={imgAlt}> */}
         </img>
     )
 
