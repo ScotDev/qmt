930f62ff56088da3283d1dfce95e2eb46a1d5810
Callum <callum.git@outlook.com>
2020-08-11 12:12:57 +0100

    .

--- next.config.js
+++ next.config.js
@@ -1,15 +1,10 @@
-// module.exports = {
-//     serverRuntimeConfig: {
-//         // Will only be available on the server side
-//         API_URL: process.env.API_URL
-//     },
-//     publicRuntimeConfig: {
-//         // Will only be available on the client side
-//         API_URL: process.env.API_URL
-//     }
-// }
 module.exports = {
-    env: {
-        API_URL: process.env.API_URL,
+    serverRuntimeConfig: {
+        // Will only be available on the server side
+        API_URL: process.env.API_URL
     },
-};
+    publicRuntimeConfig: {
+        // Will only be available on the client side
+        API_URL: process.env.API_URL
+    }
+}
