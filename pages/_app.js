5c3001625b0b52548b22f8c4f57d1e0d0815a38c
Callum <callum.git@outlook.com>
2020-08-11 14:59:01 +0100

    Added theme colour, updated manifest

--- pages/_app.js
+++ pages/_app.js
@@ -20,7 +20,7 @@ export default function MyApp({ Component, pageProps }) {
   return <>
     <Head>
       <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css" />
-      <link rel="manifest" href="/manifest.webmanifest"></link>
+      <link rel="manifest" href="/site.webmanifest"></link>
       <meta name="theme-color" content="#111111"></meta>
     </Head>
 
