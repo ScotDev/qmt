0022b12543e1749028deabfe25bdf5262eb6259e
Callum <callum.git@outlook.com>
2020-08-11 14:07:53 +0100

    Enabled support for images

--- pages/components/Header.js
+++ pages/components/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Header() {
     return (
         <div className="header">
-            <img src="/images/logo.webp" className="header-img"></img>
+            <img src="/images/logo.png" className="header-img"></img>
             <div className="header-text">the quarantine mixtape</div>
         </div>
     )
