e2f313267d6ab6d1482b7597c2976452c5812330
Callum <callum.git@outlook.com>
2020-08-16 09:33:19 +0100

    Added pagination to articles and reviews, changed btn styling

--- pages/articles/index.js
+++ pages/articles/index.js
@@ -1,13 +1,18 @@
 import Head from 'next/head';
 import Link from 'next/link';
 import getConfig from 'next/config';
+import { useRouter } from 'next/router';
+
 import axios from 'axios';
 import Moment from 'react-moment';
 
 import Error from '../components/Error'
 
 
-export default function articles({ articles }) {
+export default function articles({ articles, page, numberOfArticles }) {
+    const router = useRouter();
+    const lastPage = Math.ceil(numberOfArticles / 10)
+
     if (!articles) {
         return <Error statusCode={404}></Error>
     }
@@ -38,22 +43,40 @@ export default function articles({ articles }) {
             </div>
 
         </div>
+
+        <div className="btn-group">
+            <button className={page <= 1 ? "btn-disabled" : "btn-primary"} disabled={page <= 1} onClick={() => router.push(`/articles?page=${page - 1}`)}>Previous</button>
+
+            <button className="btn-info">{page}</button>
+
+            <button className={page >= lastPage ? "btn-disabled" : "btn-secondary"} disabled={page >= lastPage} onClick={() => router.push(`/articles?page=${page + 1}`)}>Next</button>
+        </div>
+
     </>
     )
 }
 
 
-
-export async function getServerSideProps() {
+export async function getServerSideProps({ query: { page = 1 } }) {
     try {
         const { publicRuntimeConfig } = getConfig();
         const API_URL = publicRuntimeConfig.API_URL;
-        const res = await axios.get(`${API_URL}/articles`)
+
+        // If page is equal to 1 then start from 0, else minus 1 and mutiply by 3
+        const start = parseInt(page) === 1 ? 0 : (parseInt(page) - 2) + 10;
+
+        const res = await axios.get(`${API_URL}/articles?_start=${start}&_limit=10`)
         const data = res.data;
+
+        const numberOfArticlesRes = await axios.get(`${API_URL}/articles/count`)
+        const numberOfArticles = numberOfArticlesRes.data;
+
 
         return {
             props: {
-                articles: data
+                articles: data,
+                numberOfArticles: numberOfArticles,
+                page: parseInt(page)
             }
         }
     } catch (error) {
--- pages/reviews/index.js
+++ pages/reviews/index.js
@@ -1,13 +1,17 @@
 import Head from 'next/head'
 import Link from 'next/link';
 import getConfig from 'next/config';
+import { useRouter } from 'next/router';
+
 import axios from 'axios';
 import Moment from 'react-moment';
 
 import Error from '../components/Error'
 import Img from '../components/Img'
 
-export default function reviews({ reviews }) {
+export default function reviews({ reviews, page, numberOfReviews }) {
+    const router = useRouter();
+    const lastPage = Math.ceil(numberOfReviews / 10)
 
     if (!reviews) {
         return <Error statusCode={404}></Error>
@@ -19,7 +23,7 @@ export default function reviews({ reviews }) {
             <title>thequarantinemixtape | Reviews</title>
         </Head>
 
-        <section className="review">
+        <div className="review">
             <h1 className="page-title">Album Reviews</h1>
             <div className="review-grid">
 
@@ -38,22 +42,38 @@ export default function reviews({ reviews }) {
 
             </div>
 
-        </section>
+        </div>
+        <div className="btn-group">
+            <button className={page <= 1 ? "btn-disabled" : "btn-primary"} disabled={page <= 1} onClick={() => router.push(`/reviews?page=${page - 1}`)}>Previous</button>
+
+            <button className="btn-info">{page}</button>
+
+            <button className={page >= lastPage ? "btn-disabled" : "btn-secondary"} disabled={page >= lastPage} onClick={() => router.push(`/reviews?page=${page + 1}`)}>Next</button>
+        </div>
     </>
     )
 }
 
 
-export async function getServerSideProps() {
+export async function getServerSideProps({ query: { page = 1 } }) {
     try {
         const { publicRuntimeConfig } = getConfig();
         const API_URL = publicRuntimeConfig.API_URL;
-        const res = await axios.get(`${API_URL}/reviews`)
+
+        // If page is equal to 1 then start from 0, else minus 1 and mutiply by 3
+        const start = parseInt(page) === 1 ? 0 : (parseInt(page) - 2) + 10;
+
+        const res = await axios.get(`${API_URL}/reviews?_start=${start}&_limit=10`)
         const data = res.data;
+
+        const numberOfReviewsRes = await axios.get(`${API_URL}/reviews/count`)
+        const numberOfReviews = numberOfReviewsRes.data;
 
         return {
             props: {
-                reviews: data
+                reviews: data,
+                numberOfReviews: numberOfReviews,
+                page: parseInt(page)
             }
         }
     } catch (error) {
