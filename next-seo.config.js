2324cdd1c0b7fcefc000f8efc8988ccddce2ff6f
Callum <callum.git@outlook.com>
2020-08-16 12:11:02 +0100

    Updated SEO image url

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,6 +1,7 @@
 export default {
     title: "The Quarantine Mixtape",
     description: "Discover new small bands and artists",
+    image: "https://bucketeer-22d646bf-cc53-432e-a8c1-70360c95d911.s3.amazonaws.com/logo_colour_368eae678b.jpg",
 
     openGraph: {
         title: "The Quarantine Mixtape",
@@ -8,8 +9,8 @@ export default {
         type: "website",
         locale: "en_GB",
         url: "https://thequarantinemixtape.com/",
-        site_name: "the quarantine mixtape",
-        image: "https://bucketeer-22d646bf-cc53-432e-a8c1-70360c95d911.s3.amazonaws.com/logo_colour_368eae678b.jpg"
+        site_name: "The Quarantine Mixtape",
+        images: [{ url: "https://bucketeer-22d646bf-cc53-432e-a8c1-70360c95d911.s3.amazonaws.com/logo_colour_368eae678b.jpg" }]
     },
     twitter: {
         handle: "@TheQuarantineMx",
