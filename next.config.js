542f5c74a625128e47a7aaf932bac514d84a81b0
Callum <callum.git@outlook.com>
2020-08-22 20:11:27 +0100

    Testing getform integration

--- next.config.js
+++ next.config.js
@@ -7,9 +7,9 @@ module.exports = {
         // Will only be available on the client side
         // Prod API
         API_URL: process.env.NEXT_PUBLIC_API_URL,
-        EMAIL_URL: process.env.NEXT_PUBLIC_FORM_EMAIL_URL
+        FORM_ENDPOINT: process.env.NEXT_PUBLIC_FORM_ENDPOINT
         // Dev API
         // API_URL: process.env.API_URL,
-        // EMAIL_URL: process.env.EMAIL_URL
+        // FORM_ENDPOINT: process.env.FORM_ENDPOINT
     }
 }
