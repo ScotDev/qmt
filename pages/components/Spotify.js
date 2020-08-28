ed473ef7473b14cdbc6007b2d202b2366d95d491
Callum <callum.git@outlook.com>
2020-08-28 18:27:41 +0100

    Hamburger menu collapses on route change

--- pages/components/Spotify.js
+++ pages/components/Spotify.js
@@ -1,4 +1,4 @@
-import SpotifyPlayer from 'react-spotify-player';
+// import SpotifyPlayer from 'react-spotify-player';
 
 
 export default function Spotify() {
@@ -19,29 +19,29 @@ export default function Spotify() {
             <div className="spotify-grid">
                 <div className="spotify-grid-item">
                     <h3 className="spotify-embed-title">Playlist</h3>
-                    {/* <iframe loading="lazy" className="spotify-embed" src="https://open.spotify.com/embed/playlist/1pUfcNaqfNUz4RZj92EQDR" frameBorder="0" allowtransparency="true" allow="encrypted-media" title="Spotify playlist" sandbox="allow-scripts"></iframe> */}
-                    <SpotifyPlayer
+                    <iframe loading="lazy" className="spotify-embed" src="https://open.spotify.com/embed/playlist/1pUfcNaqfNUz4RZj92EQDR" frameBorder="0" allowtransparency="true" allow="encrypted-media" title="Spotify playlist" sandbox="allow-scripts"></iframe>
+                    {/* <SpotifyPlayer
                         className="spotify-embed"
                         uri="spotify:playlist:1pUfcNaqfNUz4RZj92EQDR"
                         // size={size}
                         loading="lazy"
                         view={view}
                         theme={theme}
-                    ></SpotifyPlayer>
+                    ></SpotifyPlayer> */}
 
                 </div>
-                <div className="spotify-grid-item">
-                    <h3 className="spotify-embed-title">Podcast</h3>
-                    {/* <iframe loading="lazy" className="spotify-embed" src="https://open.spotify.com/embed/playlist/1pWvASC1XJkMvHrXlaB6GZ" frameBorder="0" allowtransparency="true" allow="encrypted-media" title="Spotify podcasts" sandbox="allow-scripts"></iframe> */}
+                {/* <div className="spotify-grid-item"> */}
+                {/* <h3 className="spotify-embed-title">Podcast</h3> */}
+                {/* <iframe loading="lazy" className="spotify-embed" src="https://open.spotify.com/embed/playlist/1pWvASC1XJkMvHrXlaB6GZ" frameBorder="0" allowtransparency="true" allow="encrypted-media" title="Spotify podcasts" sandbox="allow-scripts"></iframe> */}
 
-                    <SpotifyPlayer
+                {/* <SpotifyPlayer
                         uri="spotify:playlist:1pWvASC1XJkMvHrXlaB6GZ"
                         // size={size}
                         view={view}
                         theme={theme}
-                    ></SpotifyPlayer>
+                    ></SpotifyPlayer> */}
 
-                </div>
+                {/* </div> */}
             </div>
 
             <a href="https://open.spotify.com/user/lv5m4f490qhizetbv10ka61sl?si=XOV0rsXzT9G80-r4m3iepg" target="_blank" alt="Spotify profile" className="cta-link" rel="noopener">See more <i className="las la-arrow-right"></i></a>
