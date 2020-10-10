b0b4b20037a4987d2a214d300da99f4c9e1f0be9
Callum <callum.git@outlook.com>
2020-10-10 20:55:24 +0100

    .

--- pages/components/Error.js
+++ pages/components/Error.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 export default function Error({ statusCode }) {
     return (
         <div className="error">
-            <h1 className="error-title">Page could not be loaded</h1>
+            <h1 className="error-title">Page could not be found</h1>
             <p className="error-status-code">{statusCode}</p>
             <Link href="/">
                 <a alt="Return to homepage" className="error-return-link">Go Back <i className="icon-arrow-right"></i></a>
