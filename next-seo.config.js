4319256436b5576acb6276fd38242e7f1bfa27ae
Callum <callum.git@outlook.com>
2021-02-14 12:53:09 +0000

    Added twitter meta tags

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,7 +1,7 @@
 export default {
     title: "The Quarantine Mixtape",
     description: "Discover new small bands and artists",
-    image: "https://bucketeer-22d646bf-cc53-432e-a8c1-70360c95d911.s3.amazonaws.com/logo_colour_368eae678b.jpg",
+    image: "https://bucketeer-5a8ae918-5ea2-4eb8-aeb4-3d222713feda.s3.eu-west-1.amazonaws.com/D_Bzg_Del8_400x400_1_8f2c6bd3ba.jpg",
 
     openGraph: {
         title: "The Quarantine Mixtape",
@@ -10,11 +10,11 @@ export default {
         locale: "en_GB",
         url: "https://thequarantinemixtape.com/",
         site_name: "The Quarantine Mixtape",
-        images: [{ url: "https://bucketeer-22d646bf-cc53-432e-a8c1-70360c95d911.s3.amazonaws.com/logo_colour_368eae678b.jpg" }]
+        images: [{ url: "https://bucketeer-5a8ae918-5ea2-4eb8-aeb4-3d222713feda.s3.eu-west-1.amazonaws.com/D_Bzg_Del8_400x400_1_8f2c6bd3ba.jpg" }]
     },
     twitter: {
         handle: "@TheQuarantineMx",
-        image: "https://bucketeer-22d646bf-cc53-432e-a8c1-70360c95d911.s3.amazonaws.com/logo_colour_368eae678b.jpg",
+        image: "https://bucketeer-5a8ae918-5ea2-4eb8-aeb4-3d222713feda.s3.eu-west-1.amazonaws.com/D_Bzg_Del8_400x400_1_8f2c6bd3ba.jpg",
         cardType: "summary_large_image"
     }
 };
